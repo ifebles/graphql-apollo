14d2b1f7eba83b19cde312082fe3e6d039639d7a
Ismael Febles <reynaldofebles@gmail.com>
2024-03-05 15:08:01 -0400

    Modifying some mock data for new field

--- server/schema/resolvers.js
+++ server/schema/resolvers.js
@@ -1,5 +1,15 @@
-const usersData = require('../MOCK_DATA.json');
+let usersData = require('../MOCK_DATA.json');
 
+
+usersData = usersData.map((m, i) => {
+  if (i % 15 !== 0)
+    return m;
+
+  return {
+    ...m,
+    friends: usersData.slice(i + 1, i + 6),
+  };
+});
 
 const resolvers = {
   Query: {
